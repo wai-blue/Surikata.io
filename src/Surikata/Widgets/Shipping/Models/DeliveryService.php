05a9e8e8025a2fd9d401e814a9491ec712690345
Bombarder70 <holespato@gmail.com>
2021-08-27 14:44:43 +0200

    Delivery, Shipments, Payments, Countries CRUDs

---
+++ src/Surikata/Widgets/Shipping/Models/DeliveryService.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace ADIOS\Widgets\Shipping\Models;
+
+class DeliveryService extends \ADIOS\Core\Model {
+  var $sqlName = "delivery_services";
+  var $lookupSqlValue = "concat({%TABLE%}.name)";
+  var $urlBase = "Shipping/DeliveryServices";
+  var $tableTitle = "Delivery services";
+  var $formTitleForInserting = "New delivery service";
+  var $formTitleForEditing = "Delivery service";
+
+  var $deliveryPluginsEnumValues = [];
+
+  public function init() {
+    $this->deliveryPluginsEnumValues = [
+      "" => "-- Select a delivery plugin --",
+    ];
+
+    foreach ($this->adios->websiteRenderer->getDeliveryPlugins() as $deliveryPlugin) {
+      $this->deliveryPluginsEnumValues[$deliveryPlugin->name] = $deliveryPlugin->name;
+    }
+  }
+
+  public function columns(array $columns = []) {
+    return parent::columns([
+      "name" => [
+        'type' => 'varchar',
+        'title' => $this->translate("Delivery company name"),
+        'required' => TRUE,
+        'show_column' => TRUE,
+      ],
+
+      "description" => [
+        'type' => 'varchar',
+        'title' => $this->translate("Delivery company description"),
+        'show_column' => TRUE,
+      ],
+
+      "logo" => [
+        'type' => 'image',
+        'title' => $this->translate("Delivery company logo"),
+        'show_column' => TRUE,
+      ],
+
+      "is_enabled" => [
+        'type' => 'boolean',
+        'title' => $this->translate("Enabled"),
+        'description' => 'If not enabled, this service will not be available at the checkout.',
+        'show_column' => TRUE,
+      ],
+
+      "connected_plugin" => [
+        'type' => 'varchar',
+        'title' => $this->translate("Connected delivery plugin"),
+        "enum_values" => $this->deliveryPluginsEnumValues,
+        'show_column' => TRUE,
+      ],
+    ]);
+  }
+
+  public function indexes($columns = []) {
+    return parent::indexes([
+      "connected_plugin" => [
+        "type" => "index",
+        "columns" => ["connected_plugin"],
+      ]
+    ]);
+  }
+
+  public function getByPluginName($pluginName) {
+    /*$item = reset($this
+      ->where([
+        ["connected_plugin", "=", $pluginName],
+        ["is_enabled", "=", TRUE]
+      ])
+      ->get()
+      ->toArray()
+    );
+
+    return (is_array($item) ? $item : FALSE);
+    */
+
+    $item = [
+      "name" => "Name: {$pluginName}",
+      "description" => "Desc: {$pluginName}",
+    ];
+
+    return (is_array($item) ? $item : FALSE);
+  }
+
+}
---
+++ src/Surikata/Widgets/Shipping/Models/ShipmentPrice.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace ADIOS\Widgets\Shipping\Models;
+
+class ShipmentPrice extends \ADIOS\Core\Model {
+  var $sqlName = "shipping_shipment_prices";
+  var $lookupSqlValue = "concat({%TABLE%}.name)";
+  var $urlBase = "Shipping/ShipmentPrices";
+  var $tableTitle = "Shipment prices";
+  var $formTitleForInserting = "New shipment price";
+  var $formTitleForEditing = "Shipment price";
+
+  var $shipmentPriceCalculationMethodEnumValues = [
+    1 => "Calculate by offer value",
+    2 => "Calculate by offer height"
+  ];
+
+  public function columns(array $columns = []) {
+    return parent::columns([
+      "id_shipment" => [
+        "type" => "lookup",
+        "title" => $this->translate("Shipment"),
+        "model" => "Widgets/Shipping/Models/Shipment",
+        "required" => TRUE,
+        "show_column" => TRUE,
+      ],
+
+      "name" => [
+        'type' => 'varchar',
+        'title' => $this->translate("Unique name"),
+        "required" => TRUE,
+        'show_column' => TRUE,
+      ],
+
+      "weight_from" => [
+        "type" => "float",
+        "title" => $this->translate("Offer weight from"),
+        "show_column" => TRUE
+      ],
+
+      "weight_to" => [
+        "type" => "float",
+        "title" => $this->translate("Offer weight to"),
+        "show_column" => TRUE
+      ],
+
+      "price_from" => [
+        "type" => "float",
+        "title" => $this->translate("Offer price start"),
+        "show_column" => TRUE
+      ],
+
+      "price_to" => [
+        "type" => "float",
+        "title" => $this->translate("Offer price to"),
+        "show_column" => TRUE
+      ],
+
+      "shipment_price_calculation_method" => [
+        'type' => 'varchar',
+        'title' => $this->translate("Shipment price calculation method"),
+        "enum_values" => $this->shipmentPriceCalculationMethodEnumValues,
+        'show_column' => TRUE,
+      ],
+
+      "shipment_price" => [
+        "type" => "float",
+        "title" => $this->translate("Shipment price"),
+        "show_column" => TRUE
+      ],
+
+    ]);
+  }
+
+  public function indexes($columns = []) {
+    return parent::indexes([
+      "shipment_index" => [
+        "type" => "index",
+        "columns" => ["id_shipment"],
+      ],
+      "unique_name" => [
+        "type" => "unique",
+        "columns" => ["name"],
+      ],
+    ]);
+  }
+
+}

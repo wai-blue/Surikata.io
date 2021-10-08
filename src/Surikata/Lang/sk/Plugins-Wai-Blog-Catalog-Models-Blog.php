0feceb5cf500bcd74a83ffa5eb28623971c925a5
Bombarder70 <holespato@gmail.com>
2021-10-08 11:30:30 +0200

    WIP 2

---
+++ src/Surikata/Lang/sk/Plugins-Wai-Blog-Catalog-Models-Blog.php
@@ -0,0 +1,12 @@
+<?php
+
+$dictionary = [
+  "Blogs" => "Blogy",
+  "Title" => "Nadpis",
+  "Content" => "Obsah",
+  "Image" => "Obrázok",
+  "Created at" => "Vytvorený v",
+  "Author" => "Autor",
+  "General" => "Všeobecné",
+  "Tags" => "Tagy"
+];
---
+++ src/Surikata/Lang/sk/Widgets-Settings-Actions-Miscellaneous.php
@@ -0,0 +1,19 @@
+<?php
+
+$dictionary = [
+  "Miscellaneous settings" => "Rôzne nastavenia",
+  "Locale" => "Miestne nastavenie",
+  "Currency symbol" => "Symbol meny",
+  "Symbol of the currency displayed in the administration panel." => "Symbol meny zobrazený na administračnom paneli.",
+  "Date format" => "Formát dátumu",
+  "Date format to be used in the administration panel." => "Formát dátumu, ktorý sa má použiť v administračnom paneli.",
+  "Use notation compatible with PHP's date() function." => "Použite zápis kompatibilný s funkciou PHP date ().",
+  "List of available options is" => "Zoznam dostupných možností je",
+  "Time format" => "Formát času",
+  "Time format to be used in the administration panel." => "Časový formát, ktorý sa má použiť v administračnom paneli.",
+  "Same rules as for date format apply." => "Platia rovnaké pravidlá ako pre formát dátumu.",
+  "Shopping cart" => "Nákupný vozík",
+  "Days to abandon shopping cart" => "Počet dní do zrušenia nákupného košíka",
+  "Number of days after which the shopping cart will be treated as abanoned." => "Počet dní, po ktorých bude nákupný košík považovaný za zrušený.",
+  "day(s)" => "dni"
+];

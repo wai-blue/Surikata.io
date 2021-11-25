701cb7937f505b164107bb6f9012752371a48a81
Peter Alaxin <peter.alaxin@wai.sk>
2021-11-25 14:25:20 +0100

    Hit fix preklad filtra v Table

--- src/Surikata/Lang/cz/Core-UI-Table.php
+++ src/Surikata/Lang/cz/Core-UI-Table.php
@@ -5,5 +5,7 @@ $dictionary = [
   "Import from CSV" => "Importovat z CSV",
   "Search" => "Vyhledávání",
   "Close" => "Zavřít",
-  "items total" => "položky celkem"
+  "items total" => "položky celkem",
+  "Records are filtered" => "Záznamy jsou filtrovány",
+  "Clear filter" => "Vymazat filtr"
 ];

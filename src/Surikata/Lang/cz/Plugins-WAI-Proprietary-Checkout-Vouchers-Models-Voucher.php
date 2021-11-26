c4c6bdfe305e94703cb974ed624be9b52e493c20
Bombarder70 <holespato@gmail.com>
2021-11-26 13:14:26 +0100

    Cz translations

---
+++ src/Surikata/Lang/cz/Plugins-WAI-Proprietary-Checkout-Vouchers-Models-Voucher.php
@@ -0,0 +1,33 @@
+<?php
+
+$dictionary = [
+  "Voucher" => "Poukázka",
+  "New voucher" => "Nová poukázka",
+  "Discount" => "Sleva",
+  "Absolute discount value, e.g. 100,- " => "Celková hodnota slevy, například. 100",
+  "Relative discount value as a percentage from the total order value, e.g. 5 %" => "Hodnota slevy v procentech, například. 5%",
+  "Valid" => "Platná", 
+  "Invalid vouchers can't be used." => "Neplatná poukázka nemůže být použita.",
+  "Vouchers" => "Poukázky",
+  "Absolute discount value" => "Absolutní hodnota slevy",
+  "Relative discount value as a percentage" => "Relative discount value as a procento",
+  "Discount method" => "Způsob slevy",
+  "Max use of voucher" => "Maximální počet použití",
+  "Valid from" => "Platnost od",
+  "The date from which the voucher is enabled for use" => "Datum, od kterého lze použít poukázku",
+  "Valid to" => "Platnost do",
+  "The date until which the voucher can be used" => "Datum, do kdy lze poukázku využít",
+  "Enabled" => "Povoleno",
+  "The voucher is enabled for use" => "Poukázka je povolena k používání",
+  "General" => "Obecné",
+  "Limitation" => "Omezení",
+  "Validity" => "Platnost",
+  "Count of voucher redeemed" => "Počet uplatnění poukázky",
+  "Count of redeemed" => "Počet uplatnění",
+  "Minimum cart value for discount" => "Minimální hodnota košíku pro uplatnění slevy",
+  "The minimum value of all products in the cart from which the voucher can be redeemed" => "Minimální hodnota všech produktů v košíku, ze které lze uplatnit poukázku",
+  "Enable limit the number of voucher uses" => "Povolit omezení počtu použití poukázky",
+  "You can limit the maximum use of the voucher" => "Maximální využití poukázky můžete omezit",
+  "Maximum number of voucher uses, e.g. 15x" => "Maximální počet použití voucheru. 15x",
+  "Remaining to be redeemed" => "Zbývající na uplatnení"
+];

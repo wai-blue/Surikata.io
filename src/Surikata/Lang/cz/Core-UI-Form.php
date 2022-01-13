e8dd97561b020bdf7d44d0fb0bc8f893a5e87415
Bombarder70 <holespato@gmail.com>
2022-01-13 12:18:10 +0100

    Delete confirm translations cz/sk desktop.js

---
+++ src/Surikata/Lang/cz/Core-UI-Form.php
@@ -0,0 +1,10 @@
+<?php
+
+$dictionary = [
+  "Are you sure to delete this record?" => "Opravdu si přejete kopírovat záznam?",
+  "Some of the required fields are empty." => "Některá povinná pole nejsou vyplněna.",
+  "You are about to delete the record. Continue?" => "Chystáte se vymazat záznam. Pokračovat?",
+  "Yes, delete the record" => "Ano, vymazat záznam",
+  "Do not delete" => "Ne, ponechat",
+  "Delete record confirmation" => "Potvrzení o vymazání záznamu"
+];
--- src/Surikata/Lang/sk/Core-UI-Form.php
+++ src/Surikata/Lang/sk/Core-UI-Form.php
@@ -3,4 +3,8 @@
 $dictionary = [
   "Are you sure to delete this record?" => "Naozaj si želáte kopírovať záznam?",
   "Some of the required fields are empty." => "Niektoré povinné polia nie sú vyplnené.",
+  "You are about to delete the record. Continue?" => "Chystáte sa vymazať záznam. Pokračovať?",
+  "Yes, delete the record" => "Áno, vymazať záznam",
+  "Do not delete" => "Nie, ponechať",
+  "Delete record confirmation" => "Potvrdenie o vymazaní záznamu"
 ];

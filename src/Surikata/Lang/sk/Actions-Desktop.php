54c815fb9a95bf726850077c0de7cfe88e9db06f
Lukas <lukas.koska@wai.sk>
2022-01-27 22:33:06 +0100

    10718 - preklady logout

--- src/Surikata/Lang/sk/Actions-Desktop.php
+++ src/Surikata/Lang/sk/Actions-Desktop.php
@@ -6,4 +6,7 @@ $dictionary = [
   "Restart" => "Reštartovať",
   "Show console" => "Otvoriť konzolu",
   "Examples of UI" => "Príklady UI",
+  "Log out" => "Odhlásiť sa",
+  "Are you sure to log out?" => "Naozaj sa chcete odhlásiť?",
+  "Do not logout" => "Neodhlasovať"
 ];

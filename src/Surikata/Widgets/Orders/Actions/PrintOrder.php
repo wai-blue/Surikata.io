d5c073d42fa6321239fa95086ee5e3979b6494eb
Bombarder70 <holespato@gmail.com>
2022-02-18 13:28:34 +0100

    Order print

--- src/Surikata/Widgets/Orders/Actions/PrintOrder.php
+++ src/Surikata/Widgets/Orders/Actions/PrintOrder.php
@@ -10,9 +10,17 @@ class PrintOrder extends \ADIOS\Core\Widget\Action {
   public function preRender() {
 
     $order = (new Order($this->adios))->getById($this->params['id']);
+    $order = (new Order($this->adios))->getExtendedData($order);
 
+    foreach ($order["ITEMS"] as $key => $item) {
+      if (is_numeric($item["id_delivery_unit"])) {
+        $invoice["ITEMS"][$key]["DELIVERY_UNIT"] = (new \ADIOS\Widgets\Settings\Models\Unit($this->adios))->getById((int)$item["id_delivery_unit"]);
+      }
+    }
+    _print_r($order);
     return [
       "order" => $order,
+      "template" => 1
     ];
   }
 }
